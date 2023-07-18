10446eaed9f250a6a8a10ed2fb1681719a292aa1
KolluriVenkataSravani <127029714+KolluriVenkataSravani@users.noreply.github.com>
2023-07-18 20:23:11 +0530

    Two Sum Problem using List

---
+++ TwoSum.py
@@ -0,0 +1,16 @@
+'''
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+'''
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        lst=[]
+        for i in range(len(nums)):
+            for j in range(i):
+                if(nums[i]+nums[j]==target):
+                    lst.append(i)
+                    lst.append(j)
+                    break
+        return lst
+        
