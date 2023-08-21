dec7423122d259d8936a6f067a825fd9355c16ab
KolluriVenkataSravani <127029714+KolluriVenkataSravani@users.noreply.github.com>
2023-08-21 23:03:06 +0530

    Missing Number Leetcode problem

---
+++ Missing Number.py
@@ -0,0 +1,10 @@
+'''
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+'''
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums=set(nums)
+        n=len(nums)
+        for i in range(n+1):
+            if  i not in nums:
+                return i
