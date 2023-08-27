f4495032a1f5706b8ad0dab86a345bf598ca01a7
KolluriVenkataSravani <127029714+KolluriVenkataSravani@users.noreply.github.com>
2023-08-27 22:41:18 +0530

    Sqrt(x) Leetcode problem

--- Sqrt(x).py
+++ Sqrt(x).py
@@ -1,1 +1,20 @@
-'
+'''
+Given a non-negative integer x, return the square root of x rounded down to 
+the nearest integer. The returned integer should be non-negative as well.
+You must not use any built-in exponent function or operator.
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+'''
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0 or x == 1:
+            return x
+        l, r = 1, x
+        while l <= r:
+            m = l + (r - l) // 2
+            if m * m == x:
+                return m
+            elif m * m < x:
+                l = m + 1
+            else:
+                r = m - 1 
+        return r
