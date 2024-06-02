6c97b4b6c0ba8e210cf8fb1e75d4319e0c6f3349
KolluriVenkataSravani <127029714+KolluriVenkataSravani@users.noreply.github.com>
2024-06-02 21:48:28 +0530

    Time: 6 ms (59.33%) | Memory: 40.3 MB (63.50%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.java
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -7,9 +7,10 @@ class Solution {
         dp[1]=0;
         for(int i=2;i<=n;i++){
             dp[i]=i;
-            for(int j=1;j<=i/2;j++){
+            for(int j=1;j<=Math.sqrt(i);j++){
                 if(i%j==0){
                     dp[i]=Math.min(dp[i],dp[j]+(i/j));
+                    dp[i]=Math.min(dp[i],dp[i/j]+j);
                 }
             }
         }
