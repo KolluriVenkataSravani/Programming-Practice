6622621c331565867fa34a99149938b32fcfbcbb
KolluriVenkataSravani <127029714+KolluriVenkataSravani@users.noreply.github.com>
2024-12-02 12:47:59 +0530

    Time: 3 ms (73.32%) | Memory: 48.3 MB (14.21%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,13 +1,40 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> ans=new ArrayList<>();
-        Map<Integer,Integer> mp=new HashMap<>();
-        int min=(int) Math.floor(nums.length/3)+1;
+        int count1=0;
+        int count2=0;
+        int e1=Integer.MIN_VALUE;
+        int e2=Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++){
-            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
-            if(mp.get(nums[i])==min){
-                ans.add(nums[i]);
+            if(count1==0 && nums[i]!=e2){
+                count1=1;
+                e1=nums[i];
+            }else if(count2==0 && nums[i]!=e1){
+                count2=1;
+                e2=nums[i];
+            }else if(e1==nums[i]){
+                count1++;
+            }else if(e2==nums[i]){
+                count2++;
+            }else{
+                count1--;
+                count2--;
             }
+        }
+        int c1=0,c2=0;
+        for(int i=0;i<nums.length;i++){
+            if(e1==nums[i]){
+                c1++;
+            }else if(e2==nums[i]){
+                c2++;
+            }
+        }
+        int min=(int) (nums.length/3)+1;
+        if(c1>=min){
+            ans.add(e1);
+        }
+        if(c2>=min){
+            ans.add(e2);
         }
         return ans;
     }
