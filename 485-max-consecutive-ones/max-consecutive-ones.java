d82955e28b60d2bb3a03ae49476cea5f5231217a
KolluriVenkataSravani <127029714+KolluriVenkataSravani@users.noreply.github.com>
2025-06-05 21:28:46 +0530

    Time: 3 ms (53.77%) | Memory: 50.1 MB (78.23%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.java
+++ 485-max-consecutive-ones/max-consecutive-ones.java
@@ -5,10 +5,11 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(nums[i]==1){
                 count++;
-                maxx=Math.max(maxx,count);
             }else{
+                maxx=Math.max(maxx,count);
                 count=0;
             }
+            maxx=Math.max(maxx,count);
         }
         return maxx;
     }
