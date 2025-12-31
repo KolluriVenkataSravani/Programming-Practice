e61ab5cbb5109e28f454b05c5c0fd19ac1b2033f
KolluriVenkataSravani <127029714+KolluriVenkataSravani@users.noreply.github.com>
2025-12-31 22:01:06 +0530

    Time: 0 ms (100.00%) | Memory: 55.4 MB (44.89%) - LeetSync

--- 290-word-pattern/word-pattern.ts
+++ 290-word-pattern/word-pattern.ts
@@ -4,20 +4,19 @@ function wordPattern(pattern: string, s: string): boolean {
     if(parray.length!==sarray.length){
         return false;
     }
-    let sp_map=Object.create(null);
-    let ps_map=Object.create(null);
+    let sp_map=new Map<string, string>();
+    let ps_map=new Map<string, string>();
     for(let i=0;i<parray.length;i++){
         const w=sarray[i];
         const p=parray[i];
-        if(ps_map[p] && ps_map[p]!==w){
+        if(ps_map.has(p) && ps_map.get(p) !==w){
             return false;
         }
-        if(sp_map[w] && sp_map[w]!==p){
+        if(sp_map.has(w) && sp_map.get(w)!==p){
             return false;
         }
-        ps_map[p]=w;
-        sp_map[w]=p;
+        ps_map.set(p,w);
+        sp_map.set(w,p);
     }
-    let newArray=parray.map(pat=>ps_map[pat]);
-    return newArray.join(" ")===sarray.join(" ");
+    return true;
 };
