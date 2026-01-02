b4d3c122fc35942f855205bd965b03b3d2f46332
KolluriVenkataSravani <127029714+KolluriVenkataSravani@users.noreply.github.com>
2026-01-02 21:03:35 +0530

    Time: 0 ms (100.00%) | Memory: 58.5 MB (68.57%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.ts
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.ts
@@ -1,11 +1,15 @@
 function repeatedNTimes(nums: number[]): number {
     const n=nums.length/2;
-    let countMap=new Map<number,number>();
+    let countSet=new Set<number>();
     for(const num of nums){
-        countMap.set(num,(countMap.get(num)|| 0)+1);
-        if(countMap.get(num)===n) return num;
+        if(countSet.has(num)) return num;
+        countSet.add(num);
     }
-
+    // let countMap=new Map<number,number>();
+    // for(const num of nums){
+    //     countMap.set(num,(countMap.get(num)|| 0)+1);
+    //     if(countMap.get(num)===n) return num;
+    // }
     // for(let i=0;i<nums.length;i++){
     //     const frequency=nums.filter(num=>num===nums[i]);
     //     if(frequency.length===n) return nums[i];
