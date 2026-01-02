96538919c04f006a6e827bdc13915b7de6841ecc
KolluriVenkataSravani <127029714+KolluriVenkataSravani@users.noreply.github.com>
2026-01-02 22:29:31 +0530

    Time: 0 ms (100.00%) | Memory: 55.7 MB (39.36%) - LeetSync

--- 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.ts
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.ts
@@ -1,14 +1,12 @@
 function countOperations(num1: number, num2: number): number {
     let count=0;
-    if(num1===0 || num2===0) return 0;
-    if(num1===num2) return 1;
     while(num1>0 && num2>0){
         if(num1>num2){
-            num1=num1-num2;  //num1=11,num2=68
-            count++;
+            count+=Math.floor(num1/num2); 
+            num1=num1%num2;   
         }else{
-            num2=num2-num1;
-            count++;
+            count+=Math.floor(num2/num1); 
+            num2=num2%num1; 
         }
     }
     return count;
