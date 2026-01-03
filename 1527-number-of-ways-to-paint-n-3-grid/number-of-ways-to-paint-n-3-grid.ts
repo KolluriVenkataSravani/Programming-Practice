cce7b3def63f3b0ccd63c8e408da827d28342caf
KolluriVenkataSravani <127029714+KolluriVenkataSravani@users.noreply.github.com>
2026-01-03 21:17:00 +0530

    Time: 5 ms (33.33%) | Memory: 56 MB (66.67%) - LeetSync

---
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.ts
@@ -0,0 +1,11 @@
+function numOfWays(n: number): number {
+    let a = 6, b = 6;
+    const MOD=1_000_000_007;
+    for (let i = 2; i <= n; i++) {
+        const a1 = (2 * a + 2 * b)%MOD;
+        const b1 = (2 * a + 3 * b)%MOD;
+        a = a1;
+        b = b1;
+    }
+    return (a+b)%MOD;
+};
