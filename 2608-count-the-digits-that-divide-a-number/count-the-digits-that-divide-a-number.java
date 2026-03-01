c7efbe9d82f76dee06cdf2f7486c86cc96eff2c6
KolluriVenkataSravani <127029714+KolluriVenkataSravani@users.noreply.github.com>
2026-03-01 21:36:21 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (94.91%) - LeetSync

---
+++ 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countDigits(int num) {
+        if(num==0) return -1;
+        int n=Math.abs(num);
+        int count=0;
+        while(n>0){
+            int rem=n%10;
+            if(num%rem==0){
+                count++;
+            }
+            n=n/10;
+        }
+        return count;
+    }
+}
